25bdd5f951b2e66d45df396dff0dcf8b7eefa794
leonardoaguirre <62442754+leonardoaguirre@users.noreply.github.com>
2022-01-18 22:20:22 -0300

    Inclui try/catch no listMessage

--- Src/Controllers/ListMessage.controller.ts
+++ Src/Controllers/ListMessage.controller.ts
@@ -1,12 +1,17 @@
 import { Request, Response } from "express";
+import { NotFound } from "../Errors/NotFound.Error";
 import { ListMessageService } from "../Services/ListMessage.service";
 
 export class ListMessageController {
     async handle(request: Request, response: Response) {
         const listMessageService = new ListMessageService()
 
-        const allMsgs = await listMessageService.execute()
-
-        return response.json(allMsgs)
+        try {
+            const allMsgs = await listMessageService.execute()
+            if (allMsgs.length < 1) return response.json(new NotFound('Message'))
+            return response.json(allMsgs)
+        } catch (error) {
+            return response.status(400).json(error)
+        }
     }
 }
